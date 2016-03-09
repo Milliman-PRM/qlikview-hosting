9ee2d17858547d516289b5af6adc5ee088fcf72a
Tom Puckett <Tom.Puckett@milliman.com>
2016-03-09 14:22:34 -0500

    Added content from new folder structure.

---
+++ Apps/Cdr/WoahCdrAggregation/BayClinicCdrAggregationSvc/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BayClinicCdrAggregationSvc
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new BcCdrAggregationSvc()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
