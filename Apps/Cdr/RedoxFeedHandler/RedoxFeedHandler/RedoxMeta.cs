dc5ca77e7622fdad060ca104e890286037353ee3
Tom Puckett <Tom.Puckett@milliman.com>
2016-03-30 16:36:00 -0400

    Initial addition of Redox solution (WIP) to source control.

---
+++ Apps/Cdr/RedoxFeedHandler/RedoxFeedHandler/RedoxMeta.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace RedoxFeedHandler
+{
+    /// <summary>
+    /// TODO Figure out how to conveniently make this serialize/deserialize to/from JSON.  
+    /// </summary>
+    public class RedoxMeta
+    {
+        private string _DataModel = "";
+        private string _EventType = "";
+        private Nullable<DateTime> _EventDateTime;
+        private bool _Test;
+        private Dictionary<string,string> _Source = new Dictionary<string, string>();
+        private Dictionary<string, string> _Message = new Dictionary<string, string>();
+        private Dictionary<string, string> _Transmission = new Dictionary<string, string>();
+        private string _FacilityCode = null;
+
+        public RedoxMeta()
+        {}
+
+        public RedoxMeta(JProperty MetaProperty)
+        {
+            if (MetaProperty.Name != "Meta")
+            {
+                throw new Exception("Can't instantiate RedoxMeta with incorrect constructor argument, property name is >" + MetaProperty.Name + "<, not 'Meta'");
+            }
+            if (MetaProperty.Value.Type != JTokenType.Object)
+            {
+                throw new Exception("Can't instantiate RedoxMeta with incorrect constructor argument, property value type is >" + MetaProperty.Value.Type.ToString() + "<, not 'Object'");
+            }
+            JObject Val = MetaProperty.Value.ToObject<JObject>();
+
+            DataModel = Val.Property("DataModel");
+            EventType = Val.Property("EventType");
+            EventDateTime = Val.Property("EventDateTime");
+            Test = Val.Property("Test");
+            Source = Val.Property("Source");
+            Message = Val.Property("Message");
+            Transmission = Val.Property("Transmission");
+            EventType = Val.Property("EventType");
+            FacilityCode = Val.Property("FacilityCode");
+        }
+
+        public JProperty DataModel
+        {
+            set {
+                if (value.Type == JTokenType.Property && value.Name == "DataModel" && value.Value.Type == JTokenType.String)
+                {
+                    _DataModel = value.Value.ToObject<string>();
+                }
+                else throw new Exception("Invalid JToken type for property DataModel");
+            }
+            get
+            {
+                return new JProperty("DataModel", _DataModel);
+            }
+        }
+
+        public JProperty EventType
+        {
+            set
+            {
+                if (value.Type == JTokenType.Property && value.Name == "EventType" && value.Value.Type == JTokenType.String)
+                {
+                    _EventType = value.Value.ToObject<string>();
+                }
+                else throw new Exception("Invalid JToken type for property EventType");
+            }
+            get
+            {
+                return new JProperty("EventType", _EventType);
+            }
+        }
+
+        public JProperty EventDateTime
+        {
+            set
+            {
+                if (value.Type == JTokenType.Property && value.Name == "EventDateTime" && value.Value.Type == JTokenType.Date)
+                {
+                    _EventDateTime = value.Value.ToObject<DateTime>();
+                }
+                else throw new Exception("Invalid JToken type for property EventDateTime");
+            }
+            get
+            {
+                return new JProperty("EventDateTime", _EventDateTime);
+            }
+        }
+
+        public JProperty Test
+        {
+            set
+            {
+                if (value.Type == JTokenType.Property && value.Name == "Test" && value.Value.Type == JTokenType.Boolean)
+                {
+                    _Test = value.Value.ToObject<bool>();
+                }
+                else throw new Exception("Invalid JToken type for property Test");
+            }
+            get
+            {
+                return new JProperty("Test", _Test);
+            }
+        }
+
+        public JProperty Source
+        {
+            set
+            {
+                _Source = new Dictionary<string, string>();
+                if (value.Type == JTokenType.Property && value.Name == "Source" && value.Value.Type == JTokenType.Object)
+                {
+                    JObject Val = value.Value.ToObject<JObject>();
+                    foreach (JProperty Prop in Val.Properties())
+                    {
+                        _Source[Prop.Name] = Prop.Value.ToObject<string>();
+                    }
+                }
+                else throw new Exception("Invalid JProperty type, name, or value type for RedoxMeta property 'Source'");
+            }
+
+            get
+            {
+                JObject ReturnObj = new JObject();
+                foreach (string Key in _Source.Keys)
+                {
+                    ReturnObj.Add(Key, _Source[Key]);
+                }
+                return new JProperty("Source", ReturnObj);
+            }
+        }
+
+        public JProperty Message
+        {
+            set
+            {
+                _Message = new Dictionary<string, string>();
+                if (value.Type == JTokenType.Property && value.Name == "Message" && value.Value.Type == JTokenType.Object)
+                {
+                    JObject Val = value.Value.ToObject<JObject>();
+                    foreach (JProperty Prop in Val.Properties())
+                    {
+                        _Message[Prop.Name] = Prop.Value.ToObject<string>();
+                    }
+                }
+                else throw new Exception("Invalid JProperty type, name, or value type for RedoxMeta property 'Message'");
+            }
+
+            get
+            {
+                JObject ReturnObj = new JObject();
+                foreach (string Key in _Message.Keys)
+                {
+                    ReturnObj.Add(Key, Int64.Parse(_Message[Key]));
+                }
+                return new JProperty("Message", ReturnObj);
+            }
+        }
+
+        public JProperty Transmission
+        {
+            set
+            {
+                _Transmission = new Dictionary<string, string>();
+                if (value.Type == JTokenType.Property && value.Name == "Transmission" && value.Value.Type == JTokenType.Object)
+                {
+                    JObject Val = value.Value.ToObject<JObject>();
+                    foreach (JProperty Prop in Val.Properties())
+                    {
+                        _Transmission[Prop.Name] = Prop.Value.ToObject<string>();
+                    }
+                }
+                else throw new Exception("Invalid JProperty type, name, or value type for RedoxMeta property 'Transmission'");
+            }
+
+            get
+            {
+                JObject ReturnObj = new JObject();
+                foreach (string Key in _Transmission.Keys)
+                {
+                    ReturnObj.Add(Key, Int64.Parse(_Transmission[Key]));
+                }
+                return new JProperty("Transmission", ReturnObj);
+            }
+        }
+
+
+        public JProperty FacilityCode
+        {
+            set
+            {
+                if (value.Type == JTokenType.Property && value.Name == "FacilityCode")
+                {
+                    switch (value.Value.Type)
+                    {
+                        case JTokenType.String:
+                            _FacilityCode = value.Value.ToObject<string>();
+                            break;
+                        case JTokenType.Null:
+                            _FacilityCode = null;
+                            break;
+                    }
+                }
+                else throw new Exception("Invalid JToken type for property FacilityCode");
+            }
+            get
+            {
+                return new JProperty("FacilityCode", _FacilityCode);
+            }
+        }
+
+
+    }
+}
+
---
+++ Apps/Cdr/RedoxFeedHandler/RedoxFeedTestClient/Form2.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Media;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Bson;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+
+namespace RedoxFeedTestClient
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+
+            comboBox1.SelectedIndex = 1;
+            comboBox1.Focus();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            using (var client = new HttpClient())
+            {
+                if (comboBox1.SelectedText == comboBox1.Text)
+                {
+                    SystemSounds.Beep.Play();
+                    comboBox1.Focus();
+                    return;
+                }
+
+                string Domain = comboBox1.GetItemText(comboBox1.SelectedItem);
+                string Uri = "RedoxFeedTest/WaterfallClinicViaRedox.ashx";
+
+                if (Domain.IndexOf("localhost") != -1)
+                {
+                    int Temp;
+                    if (!int.TryParse(textPort.Text, out Temp))
+                    {
+                        SystemSounds.Beep.Play();
+                        textPort.Focus();
+                        return;
+                    }
+                    Domain = Domain.Insert(Domain.Length - 1, ":" + textPort.Text);
+                    Uri = Uri.Remove(0, "RedoxFeedTest/".Length);
+                }
+
+                client.BaseAddress = new Uri(Domain);   //  "http://PRMDev03.cloudapp.net/"
+                client.DefaultRequestHeaders.Accept.Clear();
+                //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Add("application-name", "RedoxEngine");
+                client.DefaultRequestHeaders.Add("verification-token", "83123D10-3436-4AC4-B479-1748DEDFE8F6");
+
+                HttpResponseMessage response;
+
+                System.Windows.Forms.Button Sender = sender.GetType() == typeof(System.Windows.Forms.Button) ? (System.Windows.Forms.Button) sender : null;
+
+                switch (Sender.Text)
+                {
+                    case "POST":
+                        string x;
+                        string Body = "{'Meta':{'DataModel':'Scheduling','EventType':'New','EventDateTime':'2016-03-23T20:01:33.304Z','Test':true,'Source':{'ID':'7ce6f387-c33c-417d-8682-81e83628cbd9','Name':'Redox Dev Tools'},'Message':{ 'ID':5565},'Transmission':{ 'ID':1125106},'FacilityCode':null},'Patient':{'Identifiers':[{'ID':'0000000001','IDType':'MR'},{'ID':'e167267c-16c9-4fe3-96ae-9cff5703e90a','IDType':'REDOX'}],'Demographics':{'FirstName':'Timothy','LastName':'Bixby','DOB':'2008-01-06','SSN':'101-01-0001','Sex':'Male','Race':'White','MaritalStatus':'Single','PhoneNumber':{'Home':'+18088675301','Office':null,'Mobile':null},'EmailAddresses':[],'Address':{'StreetAddress':'4762 Hickory Street','City':'Monroe','State':'WI','ZIP':'53566','County':'Green','Country':'US'}},'Notes':[]},'Visit':{'VisitNumber':'1234','VisitDateTime':'2016-03-24T17:51:22.033Z','Duration':15,'Reason':'Checkup','Instructions':null,'AttendingProvider':{'ID':4356789876,'IDType':'NPI','FirstName':'Pat','LastName':'Granite','Credentials':['MD'],'Address':{'StreetAddress':'123 Main St.','City':'Madison','State':'WI','ZIP':'53703','County':'Dane','Country':'USA'},'Location':{'Type':null,'Facility':null,'Department':null},'PhoneNumber':{'Office':null}},'ConsultingProvider':{'ID':null,'IDType':null,'FirstName':null,'LastName':null,'Credentials':[],'Address':{'StreetAddress':null,'City':null,'State':null,'ZIP':null,'County':null,'Country':null},'Location':{'Type':null,'Facility':null,'Department':null},'PhoneNumber':{'Office':null}},'ReferringProvider':{'ID':null,'IDType':null,'FirstName':null,'LastName':null,'Credentials':[],'Address':{'StreetAddress':null,'City':null,'State':null,'ZIP':null,'County':null,'Country':null},'Location':{'Type':null,'Facility':null,'Department':null},'PhoneNumber':{'Office':null}},'Location':{'Type':null,'Facility':null,'Department':'3S'},'Diagnoses':[{'Code':'034.0','Codeset':'ICD-9','Name':'Strepthroat'}]}}";
+
+                        JObject Payload = JObject.Parse(Body);
+                        response = await client.PostAsJsonAsync(Uri, Payload);
+                        x = await response.Content.ReadAsStringAsync();
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            /*
+                            Uri gizmoUrl = response.Headers.Location;
+                            // HTTP PUT
+                            gizmo.Price = 80;   // Update price
+                            response = await client.PutAsJsonAsync(gizmoUrl, gizmo);
+                            x = await response.Content.ReadAsStringAsync();
+                            // HTTP DELETE
+                            response = await client.DeleteAsync(gizmoUrl);
+                            x = await response.Content.ReadAsStringAsync();
+                            */
+                        }
+                        break;
+
+                    case "GET":
+                        string ChallengeString = Guid.NewGuid().ToString();
+                        Uri = Uri + "?challenge=" + ChallengeString;
+                        response = await client.GetAsync(Uri);
+                        if (response.IsSuccessStatusCode)
+                        {
+                            string RspContentString = await response.Content.ReadAsStringAsync();
+                            if (RspContentString != ChallengeString)
+                            {
+                                string MsgString = "response Content\n" + RspContentString + "\ndoes not match original challenge string:\n" + ChallengeString;
+                                MessageBox.Show(MsgString);
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+
+    }
+
+
