175020157681fd457d49d76d6b1030594b1e2b5c
Tom Puckett <Tom.Puckett@milliman.com>
2016-05-17 16:52:41 -0400

    Added service threading setup and installer for Bay Clinic installer.

--- Apps/Cdr/WoahCdrAggregation/BayClinicCdrAggregationSvc/BcCdrAggregationSvc.cs
+++ Apps/Cdr/WoahCdrAggregation/BayClinicCdrAggregationSvc/BcCdrAggregationSvc.cs
@@ -6,12 +6,16 @@ using System.Diagnostics;
 using System.Linq;
 using System.ServiceProcess;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using BayClinicCdrAggregationLib;
 
 namespace BayClinicCdrAggregationSvc
 {
     public partial class BcCdrAggregationSvc : ServiceBase
     {
+        BcCdrAggregationLib LibInstance = new BcCdrAggregationLib();
+
         public BcCdrAggregationSvc()
         {
             InitializeComponent();
@@ -19,6 +23,12 @@ namespace BayClinicCdrAggregationSvc
 
         protected override void OnStart(string[] args)
         {
+            for (int DelayCount = 0; DelayCount < 9; DelayCount++)
+            {
+                Thread.Sleep(1000);
+            }
+            LibInstance.StartThread();
+            bool Ended = LibInstance.EndThread();
         }
 
         protected override void OnStop()
---
+++ Apps/Cdr/WoahCdrAggregation/BayClinicCdrAggregationSvc/ProjectInstaller.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace BayClinicCdrAggregationSvc
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.WoahBayClinicAggregationSvcProcessInstaller = new System.ServiceProcess.ServiceProcessInstaller();
+            this.WoahBayClinicAggregationSvcInstaller = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // WoahBayClinicAggregationSvcProcessInstaller
+            // 
+            this.WoahBayClinicAggregationSvcProcessInstaller.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+            this.WoahBayClinicAggregationSvcProcessInstaller.Password = null;
+            this.WoahBayClinicAggregationSvcProcessInstaller.Username = null;
+            // 
+            // WoahBayClinicAggregationSvcInstaller
+            // 
+            this.WoahBayClinicAggregationSvcInstaller.Description = "Aggregates raw data from Bay Clinic\'s daily Cerner ambulatory export";
+            this.WoahBayClinicAggregationSvcInstaller.ServiceName = "WoahBayClinicAggregationSvc";
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.WoahBayClinicAggregationSvcProcessInstaller,
+            this.WoahBayClinicAggregationSvcInstaller});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller WoahBayClinicAggregationSvcProcessInstaller;
+        private System.ServiceProcess.ServiceInstaller WoahBayClinicAggregationSvcInstaller;
+    }
+}
