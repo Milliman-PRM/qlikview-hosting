c9f086f580692f0e5cc3c95b2aebc3e1b5b50c71
afsheen.khan <afsheen.khan@milliman.com>
2016-06-08 13:05:40 -0400

    Initlial Work - Password Rest Utility
    Former-commit-id: 24e2066e0e237df1bc2003d608ad6d590388a17d

---
+++ Apps/PasswordRestUtility/PasswordRestUtility/PasswordUtilityProcessor/BaseProcessor.cs
@@ -0,0 +1,36 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Security;
+
+namespace PasswordUtilityProcessor
+{
+    public class BaseFileProcessor
+    {
+        //switch to using the generic logger
+        public static log4net.ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public BaseFileProcessor()  {  }
+        
+        #region Error Log     
+        //Logs error. It will create file at the directory if file does not exist   
+        public static void LogError(Exception ex, string message)
+        {
+            if (!string.IsNullOrEmpty(message))
+            {
+                log.Error("||-||" + message + "||-|| ", ex);
+            }
+            else
+            {
+                log.Error("||-||", ex);
+            }
+        }
+        #endregion
+    }
+}
