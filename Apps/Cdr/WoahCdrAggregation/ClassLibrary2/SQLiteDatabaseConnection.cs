5dbad0296b4843b2c47024331b81c2f677985726
Nick Zenobi <Nicholas.Zenobi@milliman.com>
2016-06-20 11:28:22 -0400

    Now always gets the most recent sql database
    Former-commit-id: 331d9fdfa9f0d840d9cf17244f41420052966855

--- Apps/Cdr/WoahCdrAggregation/ClassLibrary2/SQLiteDatabaseConnection.cs
+++ Apps/Cdr/WoahCdrAggregation/ClassLibrary2/SQLiteDatabaseConnection.cs
@@ -6,22 +6,29 @@ using System.Threading.Tasks;
 using Devart.Data;
 using Devart.Data.SQLite;
 using System.Data;
+using System.IO;
 
 namespace SQLiteConnect
 {
     public class SQLiteDatabaseConnection
     {
-        string columns = "mem_name, dob";
-        string table = "member";
-        string source = @"K:\PHI\0273WOH\3.005-0273WOH06\5-Support_files\Data_Thru_201604_M5_QM\035_Staging_Membership\Members_0273WOH_3.005-0273WOH_v5.11.2_00_Data20160426_v20160524183418.sqlite";
+        string Columns = "mem_name, dob";
+        string Table = "member";
+        string Directory = @"K:\PHI\0273WOH\3.005-0273WOH06\5-Support_files\";
+        string source = "";
         public SQLiteDataReader makeConnection() {
+
+            var getDirectory = new DirectoryInfo(Directory);
+            var mostRecentDirectory = getDirectory.GetDirectories().OrderByDescending(f => f.Name).First();
+
+            source = Directory + mostRecentDirectory + @"\035_Staging_Membership\Members_3.005-0273WOH06.sqlite";
 
             SQLiteConnection myConn = new SQLiteConnection("Data Source=" + source);
             myConn.Open();
 
             SQLiteCommand cmd = myConn.CreateCommand();
 
-            cmd.CommandText = "select " + columns + " from " + table;
+            cmd.CommandText = "select " + Columns + " from " + Table;
             cmd.CommandType = CommandType.Text;
 
             SQLiteDataReader reader = cmd.ExecuteReader();
@@ -31,3 +38,4 @@ namespace SQLiteConnect
 
     }
 }
+
