e8bc7fc7e9a1717218ee9dfc3503d0e687bb1223
afsheen.khan <afsheen.khan@milliman.com>
2016-08-15 16:39:29 -0400

    Updated the script
    Former-commit-id: bc3f75a2611b9f660a1d9fd90d759b37fb11687a

--- Apps/SystemReporting/Milliman.Data/Migrations/add_Report_Type_To_Database.sql
+++ Apps/SystemReporting/Milliman.Data/Migrations/add_Report_Type_To_Database.sql
@@ -1,5 +1,9 @@
 ﻿
---Creates the reporttype table 
+ --/*********************************************************************************************************************/--
+  --/*** This code has been tested and verified and must be used						AK 08/15/2016	*****************/--
+   --/*********************************************************************************************************************/--
+
+--1 Creates the reporttype table 
 CREATE TABLE reporttype
 (
   id serial NOT NULL,
@@ -14,7 +18,9 @@ WITH (
 ALTER TABLE public.reporttype
   OWNER TO postgres;
 
---Populate the report type table
+ --/*********************************************************************************************************************/--
+
+--2. Populate the report type table
 INSERT INTO reporttype (id, type, keywords) VALUES (1, 'CARE_COORDINATOR_REPORT', 'CARE,COORDINATOR');
 INSERT INTO reporttype (id, type, keywords) VALUES (2, 'COST_MODEL_DASHBOARD', 'COST,MODEL');
 INSERT INTO reporttype (id, type, keywords) VALUES (3, 'PRCA', 'PRCA');
@@ -36,34 +42,43 @@ INSERT INTO reporttype (id, type, keywords) VALUES (18, 'NY_DEV_BPCI_CAM', 'DEV,
 INSERT INTO reporttype (id, type, keywords) VALUES (19, 'NY_LIVE_BPCI_CAM', 'LIVE,BPCI,CAM');
 
 
-CREATE TABLE temp
-(
-  id integer NOT NULL DEFAULT nextval('report_id_seq'::regclass),
-  reportname character varying(100) NOT NULL,
-  reportdescription character varying(100),
-  adddate timestamp with time zone,
-  fk_report_type_id integer,
-  CONSTRAINT pk_reports_id PRIMARY KEY (id),
-  CONSTRAINT fk_report_typse_id FOREIGN KEY (fk_report_type_id)
-      REFERENCES public.reporttype (id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION,
-  CONSTRAINT uq_reports_reportname UNIQUE (reportname)
-)
-WITH (
-  OIDS=FALSE
-);
-ALTER TABLE public.reporttype
-  OWNER TO postgres;
+ --/*********************************************************************************************************************/--
 
---Alter the report table to accomodate the report type field and create foreign key reference
+--3. Alter the report table to accomodate the report type field and create foreign key reference
   ALTER TABLE public.report
     DROP COLUMN reporttype,
-    ADD fk_report_type_id INTEGER,
-    ADD CONSTRAINT fk_report_type_id FOREIGN KEY(fk_report_type_id) REFERENCES public.reporttype (id)
+	ADD fk_report_type_id INTEGER,
+		ADD CONSTRAINT fk_report_type_id FOREIGN KEY(fk_report_type_id) REFERENCES public.reporttype (id)
                ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 
---Create the function to update all of the pre exsisting reports
+
+--4. Create Temp Table to Update Report Data
+CREATE TABLE public.report_temp (LIKE public.report INCLUDING ALL);
+ALTER TABLE public.report_temp ALTER id DROP DEFAULT;
+CREATE SEQUENCE report_temp_id_seq;
+INSERT INTO public.report_temp SELECT * FROM public.report order by id;
+SELECT setval('report_temp_id_seq', (SELECT max(id) FROM public.report), true);
+ALTER TABLE public.report ALTER id SET DEFAULT nextval('report_id_seq');
+
+--5. Create Temp Table to qvauditlog
+CREATE TABLE public.qvauditlog_temp (LIKE public.qvauditlog INCLUDING ALL);
+ALTER TABLE public.qvauditlog_temp ALTER id DROP DEFAULT;
+CREATE SEQUENCE qvauditlog_temp_id_seq;
+INSERT INTO public.qvauditlog_temp SELECT * FROM public.qvauditlog order by id;
+SELECT setval('qvauditlog_temp_id_seq', (SELECT max(id) FROM public.qvauditlog), true);
+ALTER TABLE public.qvauditlog ALTER id SET DEFAULT nextval('qvauditlog_id_seq');
+
+--6. Create Temp Table to qvsessionlog Data
+CREATE TABLE public.qvsessionlog_temp (LIKE public.qvsessionlog INCLUDING ALL);
+ALTER TABLE public.qvsessionlog_temp ALTER id DROP DEFAULT;
+CREATE SEQUENCE qvsessionlog_temp_id_seq;
+INSERT INTO public.qvsessionlog_temp SELECT * FROM public.qvsessionlog order by id;
+SELECT setval('qvsessionlog_temp_id_seq', (SELECT max(id) FROM public.qvsessionlog), true);
+ALTER TABLE public.qvsessionlog ALTER id SET DEFAULT nextval('qvsessionlog_id_seq');
+ --/*********************************************************************************************************************/--
+
+--6. Create the function to update all of the pre exsisting reports
 -- Function: public.report_type_update()
 
 -- DROP FUNCTION public.report_type_update();
@@ -72,10 +87,8 @@ CREATE OR REPLACE FUNCTION public.report_type_update()
   RETURNS integer AS
 $BODY$
 
-
-
 DECLARE
-	r report;
+	r report_temp;
 	rt reporttype;
 	a qvauditlog;
 	l qvauditlog;
@@ -91,21 +104,21 @@ BEGIN
 	it := 0;			--This will keep track of how many records are updated
 	Empty_String = NULL;
 	--Goes through every report and attempts to match the reportname to the report type
-	FOR r IN SELECT * from public.report ORDER BY id
+	FOR r IN SELECT * from public.report_temp ORDER BY id
 	LOOP
 		holding_id := NULL;
 		report_name := r.reportname;	
 		IF report_name NOT LIKE '% %' AND LENGTH(report_name) = 32 THEN
 			
 			--If a GUID then go through the qvauditlog to match the GUID to a report
-			FOR a IN SELECT * FROM public.qvauditlog WHERE document LIKE '%' || report_name || '%' LIMIT 1
+			FOR a IN SELECT * FROM public.qvauditlog_temp WHERE document LIKE '%' || report_name || '%' LIMIT 1
 			LOOP
 				group_name := substring(a.document from 0 for position('REDUCEDCACHEDQVWS' in a.document));
 				group_name := replace (group_name, '\', '\\');		--Deal with those pesky escape charachters
 			END LOOP;
 			
 			--After we get the group directory we find the root report name of that directory. This is the name we will match to a type
-			FOR l IN SELECT * FROM public.qvauditlog WHERE document LIKE '%' || group_name || '%' AND document NOT LIKE '%REDUCEDCACHEDQVWS%' LIMIT 1
+			FOR l IN SELECT * FROM public.qvauditlog_temp WHERE document LIKE '%' || group_name || '%' AND document NOT LIKE '%REDUCEDCACHEDQVWS%' LIMIT 1
 			LOOP
 				group_name := replace(group_name, '\\', '\');
 
@@ -121,39 +134,25 @@ BEGIN
 		--Replace some undesirable charachters or strings that could be in the report name
 		parsed_report_name := '';
 		parsed_report_name := replace(report_name, '- ', '');
-
+		parsed_report_name := replace(parsed_report_name, 'REPORTING ', ''); --it was in commit# c37a55c9
+		
 		--Attempt to match the report name to any of the keywords in the report type table
 		FOR rt IN SELECT * FROM public.reporttype
 		LOOP
-			
 			parsed_keywords := '';
-
 			parsed_keywords := replace(rt.keywords, ',', ' ');
 			parsed_keywords := trim(both ' ' from parsed_keywords);		--Turn the keywords into a string that can match up with the report name
 
 			IF parsed_report_name LIKE '%' || parsed_keywords || ' %' OR parsed_report_name = parsed_keywords THEN
 				it := it + 1;
-				holding_id = rt.id;
-				--update public.report set fk_report_type_id = rt.id where id = r.id;		--When a match is found we update the table
+				UPDATE public.report_temp set fk_report_type_id = rt.id where id = r.id;		--When a match is found we update the table
 			END IF;
-				
+
 		END LOOP;
-		--update public.report set fk_report_type_id = temp_id where id = r.id;
-		INSERT INTO public.temp (id, reportname, reportdescription, adddate, fk_report_type_id) VALUES (r.id, r.reportname, Empty_String, r.adddate, holding_id);
-	END LOOP;
+	END LOOP;	
 RETURN it;			--Returns the amount of fields with new report types
 
-
-
 END;
-
-
-
-
-
-
-
-
 $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
@@ -161,152 +160,34 @@ ALTER FUNCTION public.report_type_update()
   OWNER TO postgres;
 
 
---Call the above function
+--/*********************************************************************************************************************/--
+--7. Call/Execute the above function
 SELECT * FROM public.report_type_update();
+--/*********************************************************************************************************************/--
+--/*********************************************************************************************************************/--
+--8. Drop the FK Contraint from qvauditlog & qvsessionlog
+ALTER TABLE public.qvauditlog DROP CONSTRAINT fk_tbl_report_id;
+ALTER TABLE public.qvsessionlog DROP CONSTRAINT fk_tbl_report_id;
 
-  ALTER TABLE public.qvsessionlog
-	DROP CONSTRAINT fk_tbl_report_id;
+--9. Truncate the report table
+TRUNCATE TABLE "report";
+--10. reset the sequence
+ALTER SEQUENCE report_id_seq RESTART WITH 1;
 
-  ALTER TABLE public.qvauditlog
-	DROP CONSTRAINT fk_tbl_report_id;
+--11. Insert into report data from temp
+INSERT INTO public.report(id, reportname, reportdescription, adddate, fk_report_type_id)
+	SELECT id, reportname, reportdescription, adddate, fk_report_type_id
+	FROM report_temp Order by id;
+	
+--12. Re-establish the qvauditlog & qvsessionlog constraint
+ALTER TABLE public.qvauditlog ADD CONSTRAINT fk_tbl_report_id FOREIGN KEY(fk_report_id) REFERENCES public.report (id);
+ALTER TABLE public.qvsessionlog ADD CONSTRAINT fk_tbl_report_id FOREIGN KEY(fk_report_id) REFERENCES public.report (id);
+--/*********************************************************************************************************************/--
+--Insert data from the Temp Report to the Origianl Report table
 
-DROP VIEW public.view_activity_log;
-DROP VIEW public.view_session_log;
-DROP TABLE public.report;
+Select * from report 
 
-ALTER TABLE public.temp
-	RENAME TO report;
-
-ALTER TABLE public.qvauditlog
-ADD  CONSTRAINT fk_tbl_report_id FOREIGN KEY (fk_report_id)
-      REFERENCES public.report (id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION;
-
-ALTER TABLE public.qvsessionlog
-ADD   CONSTRAINT fk_tbl_report_id FOREIGN KEY (fk_report_id)
-      REFERENCES public.report (id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION;
-	-- View: public.view_session_log
-
--- DROP VIEW public.view_session_log;
-
-CREATE OR REPLACE VIEW public.view_session_log AS 
- WITH sessions AS (
-         SELECT qvsessionlog.id,
-            tstzrange(qvsessionlog.useraccessdatetime,
-                CASE
-                    WHEN qvsessionlog.sessionendreason::text = 'SESSIONTIMEOUT'::text THEN qvsessionlog.useraccessdatetime + qvsessionlog.sessionduration::interval - '00:30:00'::interval
-                    ELSE qvsessionlog.useraccessdatetime + qvsessionlog.sessionduration::interval
-                END, '[]'::text) AS sessionrange,
-            qvsessionlog.fk_user_id,
-            qvsessionlog.fk_group_id,
-            qvsessionlog.fk_report_id
-           FROM qvsessionlog
-          ORDER BY qvsessionlog.fk_user_id, qvsessionlog.fk_group_id, qvsessionlog.fk_report_id, (tstzrange(qvsessionlog.useraccessdatetime,
-                CASE
-                    WHEN qvsessionlog.sessionendreason::text = 'SESSIONTIMEOUT'::text THEN qvsessionlog.useraccessdatetime + qvsessionlog.sessionduration::interval - '00:30:00'::interval
-                    ELSE qvsessionlog.useraccessdatetime + qvsessionlog.sessionduration::interval
-                END, '[]'::text))
-        ), merged_sessions AS (
-         SELECT sessions.fk_user_id,
-            sessions.fk_group_id,
-            sessions.fk_report_id,
-            merge_ranges(array_agg(sessions.sessionrange)) AS mergedrange
-           FROM sessions
-          GROUP BY sessions.fk_user_id, sessions.fk_group_id, sessions.fk_report_id
-        ), dataset AS (
-         SELECT min(sessions.id) AS first_session,
-            max(sessions.id) AS last_session,
-            count(sessions.id) AS included_sessions,
-            merged.mergedrange,
-            merged.fk_user_id,
-            merged.fk_group_id,
-            merged.fk_report_id
-           FROM merged_sessions merged
-             JOIN sessions ON merged.fk_user_id = sessions.fk_user_id AND merged.fk_group_id = sessions.fk_group_id AND merged.fk_report_id = sessions.fk_report_id AND merged.mergedrange && sessions.sessionrange
-          GROUP BY merged.fk_user_id, merged.fk_group_id, merged.fk_report_id, merged.mergedrange
-        )
- SELECT dataset.first_session AS sessionid,
-    dataset.included_sessions,
-        CASE
-            WHEN report.reportname::text ~~ '%CARE COORDINATOR REPORT%'::text THEN 'Care Coordinator Report'::text
-            WHEN report.reportname::text ~~ '%COST MODEL DASHBOARD%'::text THEN 'Cost Model Dashboard'::text
-            WHEN report.reportname::text ~~ '%PRCA%'::text THEN 'PRCA Report'::text
-            WHEN report.reportname::text ~~ '%LIVE BPCI - CAM%'::text THEN 'Live BPCI - CAM'::text
-            WHEN report.reportname::text ~~ '%LIVE BPCI - CJR%'::text THEN 'Live BPCI - CJR'::text
-            WHEN report.reportname::text ~~ '%LIVE BPCI - PREMIER%'::text THEN 'Live BPCI - Premier'::text
-            WHEN report.reportname::text ~~ '%LIVE BPCI - USPI%'::text THEN 'Live BPCI - USPI'::text
-            WHEN report.reportname::text ~~ '%OHIO FINANCIAL DASHBOARD%'::text THEN 'Ohio Financial Dashboard'::text
-            WHEN report.reportname::text ~~ '%CAPITATION DASHBOARD%'::text THEN 'Capitation Dashboard'::text
-            WHEN report.reportname::text ~~ '%ENCOUNTER QUALITY DASHBOARD%'::text THEN 'Encounter Quality Dashboard'::text
-            WHEN report.reportname::text ~~ '%PIHP SUBMITTED ENCOUNTER DASHBOARD%'::text THEN 'PIHP Submitted Encounter Dashboard'::text
-            WHEN report.reportname::text ~~ '%GAP.QVW%'::text THEN 'GAP'::text
-            WHEN report.reportname::text ~~ '%SOC WC%'::text THEN 'SOC WC'::text
-            WHEN report.reportname::text ~~ '%LOAD TABLES DVW.QVW'::text THEN 'Load Tables DVW'::text
-            WHEN report.reportname::text ~~ '%VERMONT POC.QVW'::text THEN 'Vermont PoC'::text
-            ELSE 'Other/Unknown'::text
-        END AS document_type,
-    lower(dataset.mergedrange) AS session_start_time,
-    upper(dataset.mergedrange) - lower(dataset.mergedrange) AS session_duration,
-        CASE
-            WHEN upper(lastsession.browser::text) ~~ '%ANDROID%'::text THEN 'Android'::text
-            WHEN upper(lastsession.browser::text) ~~ '%CHROME%'::text THEN 'Chrome'::text
-            WHEN upper(lastsession.browser::text) ~~ '%MSIE%'::text THEN 'MSIE'::text
-            WHEN upper(lastsession.browser::text) ~~ '%FIREFOX%'::text OR upper(lastsession.browser::text) ~~ '%GECKO%'::text THEN 'Firefox'::text
-            WHEN upper(lastsession.browser::text) ~~ '%SAFARI%'::text THEN 'Safari'::text
-            ELSE 'Unknown'::text
-        END AS browser_type,
-        CASE
-            WHEN upper(lastsession.browser::text) ~~ '%CHROME%'::text THEN 'Chrome'::character varying
-            WHEN upper(lastsession.browser::text) ~~ '%GECKO%'::text THEN replace(lastsession.browser::text, 'GECKO'::text, 'Firefox'::text)::character varying
-            WHEN lastsession.browser::text = 'msie'::text THEN 'MSIE'::character varying
-            WHEN lastsession.browser::text = 'safari'::text THEN 'Safari'::character varying
-            WHEN lastsession.browser::text = ANY (ARRAY[''::character varying::text, NULL::character varying::text]) THEN 'Unknown'::character varying
-            ELSE lastsession.browser
-        END AS browser_version,
-    dataset.fk_user_id AS userid,
-    dataset.fk_group_id AS groupid,
-    dataset.fk_report_id AS reportid
-   FROM dataset
-     JOIN report ON dataset.fk_report_id = report.id
-     JOIN qvsessionlog lastsession ON dataset.last_session = lastsession.id
-  ORDER BY dataset.included_sessions DESC;
-
-ALTER TABLE public.view_session_log
-  OWNER TO postgres;
-
-
-  -- View: public.view_activity_log
-
--- DROP VIEW public.view_activity_log;
-
-CREATE OR REPLACE VIEW public.view_activity_log WITH (security_barrier=true) AS 
- WITH fetch_names AS (
-         SELECT qvauditlog.id,
-            selected_fields.field_name,
-            char_length(selected_fields.field_name) = max(char_length(selected_fields.field_name)) OVER (PARTITION BY qvauditlog.id ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS longest_name
-           FROM qvauditlog
-             JOIN selected_fields ON qvauditlog.message ~~ (selected_fields.field_name || '%'::text)
-          WHERE qvauditlog.eventtype::text = ANY (ARRAY['Selection'::character varying::text, 'Bookmark Selection'::character varying::text])
-        ), sessions AS (
-         SELECT view_session_log.sessionid,
-            view_session_log.userid,
-            view_session_log.groupid,
-            view_session_log.reportid,
-            tstzrange(view_session_log.session_start_time, view_session_log.session_start_time + view_session_log.session_duration, '[]'::text) AS session_range
-           FROM view_session_log
-        )
- SELECT sessions.sessionid,
-    log.useraccessdatetime AS actiondatetime,
-    log.eventtype AS event_type,
-        CASE
-            WHEN (log.eventtype::text = ANY (ARRAY['Selection'::character varying::text, 'Bookmark Selection'::character varying::text])) AND fetch_names.field_name IS NOT NULL THEN fetch_names.field_name
-            WHEN (log.eventtype::text = ANY (ARRAY['Selection'::character varying::text, 'Bookmark Selection'::character varying::text])) AND log.message <> 'Clear All'::text THEN 'Unmatched Field'::text
-            ELSE log.message
-        END AS event_message
-   FROM qvauditlog log
-     LEFT JOIN fetch_names ON log.id = fetch_names.id AND fetch_names.longest_name = true
-     JOIN sessions ON log.fk_user_id = sessions.userid AND log.fk_group_id = sessions.groupid AND log.fk_report_id = sessions.reportid AND log.useraccessdatetime <@ sessions.session_range;
-
-ALTER TABLE public.view_activity_log
-  OWNER TO postgres;
+--13. Drop temp table
+Drop Table report_temp
+Drop Table qvauditlog_temp
+Drop Table qvsessionlog_temp
