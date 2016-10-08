23e918a5ef3e8be241335c51766ba9474c763e67
Van W Nanney <van.nanney@milliman.com>
2016-10-07 21:53:15 -0400

    Updated to turn off SSL check on localhost call using 127.0.0.1
    Former-commit-id: 386a085e1073177bc2ac95d21124e8fe18af2988

--- WebApps/MillFrame/MillimanClientPublisher/Global.asax.cs
+++ WebApps/MillFrame/MillimanClientPublisher/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Net;
 using System.Web;
 using System.Net.Security;
 using System.Configuration;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ClientPublisher
 {
@@ -16,6 +17,23 @@ namespace ClientPublisher
             Log("Loaded user repo");
             MillimanCommon.SuperGroup.GetInstance(); //this will cause it to load
             Log("Loaded super groups");
+
+            // Set the server certificate validation callback.
+            ServicePointManager.ServerCertificateValidationCallback = ValidateRemoteServerCertificate;
+        }
+
+        /// <summary>
+        /// Verifies the remote Secure Sockets Layer (SSL) certificate used for authentication.
+        /// </summary>
+        /// <param name="sender">An object that contains state information for this validation.</param>
+        /// <param name="certificate">The certificate used to authenticate the remote party.</param>
+        /// <param name="chain">The chain of certificate authorities associated with the remote certificate.</param>
+        /// <param name="sslPolicyErrors">One or more errors associated with the remote certificate.</param>
+        /// <returns>A System.Boolean value that determines whether the specified certificate is accepted for authentication.</returns>
+        private static bool ValidateRemoteServerCertificate(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            // NOTE: This is a test application with self-signed certificates, so all certificates are trusted.
+            return true;
         }
 
         public static string GetParameter(string RawURL, string Parameter)
