ceb5293acbd74de7237b090e2296ae3ee1520e1d
afsheen.khan <afsheen.khan@milliman.com>
2016-12-16 12:52:18 -0500

    Updated Code with Van Recommendations
    Former-commit-id: bcf7eb0bc3cca81d4741adfe23b9c41cacc8f75e

--- WebApps/MillFrame/MillimanCommon/MillimanHelper.cs
+++ WebApps/MillFrame/MillimanCommon/MillimanHelper.cs
@@ -9,156 +9,45 @@ namespace MillimanCommon
 {
     public class MillimanHelper
     {
-        #region UserAdmin
-        #region UserName Enrty Validation
-
         public static List<string> ValidateUserNameInput(string inputValue)
         {
             var errorsList = new List<string>();
-            if (!string.IsNullOrEmpty(inputValue))
+            
+            //valid email address can only contain 1 AT sign
+            if (inputValue.Count(element => element == '@') != 1)
             {
-                errorsList = CheckUserNameInput(inputValue);
+                errorsList.Add("Valid email addresses must contain a single '@' character.");
+                return errorsList;
             }
-
-            return errorsList;
-
-        }
-
-        private static List<string> CheckUserNameInput(string inputValue)
-        {
-            var errorsList = new List<string>();
-            var errorMsg = string.Empty;
-            //get the occurance of @ - if there is no @ then ignore
-            if (inputValue.IndexOf('@') > -1)
+            
+            //email address must fit the form LOCAL@SERVER format
+            List<string> EmailSplit = inputValue.Split(new char[] { '@' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            if (EmailSplit.Count() != 2)
             {
-                //do nothing, this condition is true
+                errorsList.Add("Valid email addresses must contain two parts seperated by a '@' character.");
+                return errorsList;
             }
-            else
+            
+            //check for invalid characters in LOCAL part of email
+            string EmailLocalAllowedChars = System.Configuration.ConfigurationManager.AppSettings["EmailLocalAllowedChars"];
+            System.Text.RegularExpressions.Regex LocalCharsRegEx = new System.Text.RegularExpressions.Regex("[^" + EmailLocalAllowedChars + "]");
+            if (LocalCharsRegEx.IsMatch(EmailSplit[0]) == true)
             {
-                errorMsg = "String does not contain @ sign.";
-                errorsList.Add(errorMsg);
+                errorsList.Add("The part of email address before the '@' character contained invalid characters. It should only contain characters from the set '" + EmailLocalAllowedChars + "'");
+                return errorsList;
             }
-
-            //check how many '@' exist
-            var countofAtSign = inputValue.Count(x => x == '@');
-            if (countofAtSign > 1)
+            
+            //check for invalid chars in SERVER part of email
+            string EmailServerAllowedChars = System.Configuration.ConfigurationManager.AppSettings["EmailServerAllowedChars"];
+            System.Text.RegularExpressions.Regex ServerCharsRegEx = new System.Text.RegularExpressions.Regex("[^" + EmailServerAllowedChars + "]");
+            if (ServerCharsRegEx.IsMatch(EmailSplit[1]) == true)
             {
-                errorMsg = "String contains more than one @ sign.";
-                errorsList.Add(errorMsg);
-            }
-
-            if (errorsList.Count > 0)
-            {
+                errorsList.Add("The part of email address after the '@' character contained invalid characters. It should only contain characters from the set '" + EmailServerAllowedChars + "'");
                 return errorsList;
             }
 
-            //first part of email before @ 
-            var localString = inputValue.Split('@')[0];
-
-            //there is empty string before @
-            if (localString.Length == 0)
-            {
-                errorMsg = "No characters before @ sign.";
-                errorsList.Add(errorMsg);
-            }
-
-            //second part of email
-            var serverString = inputValue.Split('@')[1];
-
-            //serverString must have few chars
-            if (serverString.Length == 0)
-            {
-                errorMsg = "No characters after @ sign.";
-                errorsList.Add(errorMsg);
-            }
-
-            //chcek for number
-            var HasNumbers = RegexUtility.CheckInputForNumbers(inputValue);
-            //check for alphabets
-            var HasAlphabets = RegexUtility.CheckInputForAlphabets(inputValue);
-            //check to see at least one character exist or one number is in string. either 1 number or 1 alphabet must exist
-            if (!HasNumbers && !HasAlphabets)
-            {
-                errorMsg = "There must be at least few characters before and after @ sign.";
-                errorsList.Add(errorMsg);
-            }
-
-            if (errorsList.Count>0)
-            {
-                return errorsList;
-            }
-
-            var hasSpecialCharacters = StringProcessingUtility.StringProcessing.CheckStringForAnySpecailChars(inputValue, SpecialChars);
-
-            //check to see if there are any specail chars in string then check if those special chars are allowed
-            if (hasSpecialCharacters) 
-            {
-                //****************** Local String Check **********************************/
-                var localStringHasSpecialChars = StringProcessingUtility.StringProcessing.CheckStringForAnySpecailChars(localString,SpecialCharsInLocalPart);
-                var nonMatchingCharsLocal = new List<string>();
-                var nonMatchingCharsServer = new List<string>();
-                if (localStringHasSpecialChars)
-                {
-                    nonMatchingCharsLocal = StringProcessingUtility.StringProcessing.CheckListforAllowedChars(RegexUtility.SplitAllWords(localString.Trim()).ToList(), string.Join("",SpecialCharsInLocalPart));
-                }
-                //****************** Server String Check **********************************/
-                var ServerStringHasSpecialChars = StringProcessingUtility.StringProcessing.CheckStringForAnySpecailChars(serverString,SpecialCharsInServerPart);
-                if (localStringHasSpecialChars)
-                {
-                    nonMatchingCharsServer = StringProcessingUtility.StringProcessing.CheckListforAllowedChars(RegexUtility.SplitAllWords(serverString.Trim()).ToList(), string.Join("", SpecialCharsInServerPart));
-                }
-
-                if (nonMatchingCharsLocal.Count > 0 || nonMatchingCharsServer.Count>0)
-                {
-                    var combinedList = nonMatchingCharsLocal.Concat(nonMatchingCharsServer);
-                    errorsList.Add("There are few unsupported specail characters in the string like " + (String.Join(",", combinedList)) + "");
-                }
-            }
             return errorsList;
-
         }
-
-        private static readonly char[] SpecialChars = GetAllSpecialChars().ToCharArray();
-        private static readonly char[] SpecialCharsInServerPart = GetAllowedCharsServerStringUserName().ToCharArray();
-        private static readonly char[] SpecialCharsInLocalPart = GetAllowedCharsInLocalStringUserName().ToCharArray();
-              
-        #endregion
-        
-        #region Config Keys Methods        
-        private static string GetAllSpecialChars()
-        {
-            //if there are values for allwoed character then display it
-            var AllSpecialChars = ConfigurationManager.AppSettings["AllSpecialChars"].ToString();
-            var BadCharactersInFriendlyNameDoubleQuote = ConfigurationManager.AppSettings["BadCharactersInFriendlyNameDoubleQuote"].ToString();
-            //remove comma from the characters than at the end add double quotes
-            var allChars = (AllSpecialChars) + (BadCharactersInFriendlyNameDoubleQuote);
-            if (!string.IsNullOrEmpty(allChars))
-                return allChars;
-
-            return string.Empty;
-        }
-
-        private static string GetAllowedCharsInLocalStringUserName()
-        {
-            //if there are values for allwoed character then display it
-            var AllowedCharsInLocalStringUserName = ConfigurationManager.AppSettings["AllowedCharsLocalPartUserName"].ToString();
-            if (!string.IsNullOrEmpty(AllowedCharsInLocalStringUserName))
-                return AllowedCharsInLocalStringUserName;
-
-            return string.Empty;
-        }
-
-        private static string GetAllowedCharsServerStringUserName()
-        {
-            //if there are values for allwoed character then display it
-            var AllowedCharsServerStringUserName = ConfigurationManager.AppSettings["AllowedCharsServerPartUserName"].ToString();
-            if (!string.IsNullOrEmpty(AllowedCharsServerStringUserName))
-                return AllowedCharsServerStringUserName;
-
-            return string.Empty;
-        }
-
-        #endregion
-        #endregion
+                
     }
 }
