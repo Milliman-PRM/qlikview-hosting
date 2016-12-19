3755c4e5b4915d1c67dd67f7e481adb7b60d1564
afsheen.khan <afsheen.khan@milliman.com>
2016-12-19 17:29:46 -0500

    Updated Code and removed extra classes
    Former-commit-id: 0d3c9a718e456af978aee4462597522459251c6a

--- WebApps/MillFrame/MillimanCommon/MillimanHelper.cs
+++ WebApps/MillFrame/MillimanCommon/MillimanHelper.cs
@@ -12,42 +12,54 @@ namespace MillimanCommon
         public static List<string> ValidateUserNameInput(string inputValue)
         {
             var errorsList = new List<string>();
-            
+
             //valid email address can only contain 1 AT sign
             if (inputValue.Count(element => element == '@') != 1)
             {
                 errorsList.Add("Valid email addresses must contain a single '@' character.");
                 return errorsList;
             }
-            
             //email address must fit the form LOCAL@SERVER format
-            List<string> EmailSplit = inputValue.Split(new char[] { '@' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            var EmailSplit = inputValue.Split(new char[] { '@' }, StringSplitOptions.RemoveEmptyEntries).ToList();
             if (EmailSplit.Count() != 2)
             {
                 errorsList.Add("Valid email addresses must contain two parts seperated by a '@' character.");
                 return errorsList;
             }
-            
+
             //check for invalid characters in LOCAL part of email
-            string EmailLocalAllowedChars = System.Configuration.ConfigurationManager.AppSettings["EmailLocalAllowedChars"];
-            System.Text.RegularExpressions.Regex LocalCharsRegEx = new System.Text.RegularExpressions.Regex("[^" + EmailLocalAllowedChars + "]");
-            if (LocalCharsRegEx.IsMatch(EmailSplit[0]) == true)
+            var EmailLocalAllowedChars = ConfigurationManager.AppSettings["EmailLocalAllowedChars"];
+            //filter out the non matching chars.
+            var filter =
+                (from w in EmailSplit[0]
+                 select new {
+                     Test=w,
+                     Count = w.ToString().ToLower().Count(c => EmailLocalAllowedChars.Contains(c))
+                 }).Where(item => item.Count == 0);//find every char that has 0 match
+
+            //if there are non-matching chars then invalidate email
+            if (filter.Count()> 0)
             {
                 errorsList.Add("The part of email address before the '@' character contained invalid characters. It should only contain characters from the set '" + EmailLocalAllowedChars + "'");
                 return errorsList;
             }
-            
-            //check for invalid chars in SERVER part of email
-            string EmailServerAllowedChars = System.Configuration.ConfigurationManager.AppSettings["EmailServerAllowedChars"];
-            System.Text.RegularExpressions.Regex ServerCharsRegEx = new System.Text.RegularExpressions.Regex("[^" + EmailServerAllowedChars + "]");
-            if (ServerCharsRegEx.IsMatch(EmailSplit[1]) == true)
+
+            var EmailServerAllowedChars = ConfigurationManager.AppSettings["EmailServerAllowedChars"];
+            filter =
+                (from w in EmailSplit[1]
+                 select new
+                 {
+                     Test = w,
+                     Count = w.ToString().ToLower().Count(c => EmailServerAllowedChars.Contains(c))
+                 }).Where(item => item.Count == 0);//find every char that has 0 match
+
+            if (filter.Count() > 0)
             {
                 errorsList.Add("The part of email address after the '@' character contained invalid characters. It should only contain characters from the set '" + EmailServerAllowedChars + "'");
                 return errorsList;
             }
 
             return errorsList;
-        }
-                
+        }                      
     }
 }
