b262298ce2c233deb928d8dcd387faf5d029f08c
Tom Puckett <Tom.Puckett@milliman.com>
2017-01-30 10:10:01 -0500

    Adjusted uptime monitor to log responses with HTTP status other than success.  Also a few minor changes to better handle unexpected situations.
    Former-commit-id: f19a2a5847d45e5908c77890eb771b569a281dba

--- Apps/PrmServerMonitor/UptimeMonitor/UptimeMonitorLib/UptimeTest.cs
+++ Apps/PrmServerMonitor/UptimeMonitor/UptimeMonitorLib/UptimeTest.cs
@@ -28,7 +28,7 @@ namespace UptimeMonitorLib
             Trace.AutoFlush = true;
 
             Client = new HttpClient();
-            Client.BaseAddress = new Uri("https://prm.milliman.com/prm/time.aspx");
+            Client.BaseAddress = new Uri("https://prm.milliman.com/prm/time.aspx?NeverFail=true");
             Client.DefaultRequestHeaders.Accept.Clear();
 
             TestStartTimer = new Timer(TimerIntervalSeconds * 1000);
@@ -86,17 +86,18 @@ namespace UptimeMonitorLib
         private void PerformUptimeTest(Object source, ElapsedEventArgs e)
         {
             //LogWriter.WriteLine("The Elapsed event was raised at " + e.SignalTime.ToString("HH:mm:ss.fff"));
+            string LogText = "";
 
-            HttpResponseMessage Response = Client.GetAsync(Client.BaseAddress).Result;
-            if (Response.IsSuccessStatusCode)
+            try
             {
+                HttpResponseMessage Response = Client.GetAsync(Client.BaseAddress).Result;
+
                 var Body = Response.Content.ReadAsStringAsync().Result;
 
                 XmlDocument Doc = new XmlDocument();
                 Doc.LoadXml(Body);
 
                 XmlNodeList a = Doc.GetElementsByTagName("span");
-                string LogText = "";
 
                 foreach (XmlNode b in a)
                 {
@@ -104,7 +105,7 @@ namespace UptimeMonitorLib
                     {
                         // put the SystemTime at front of the string and Status at the back
                         case "Status":
-                            byte[] DecodedStatus = System.Convert.FromBase64String(b.InnerText);
+                            //byte[] DecodedStatus = System.Convert.FromBase64String(b.InnerText);
                             string x = new CopyOfMillimanCommon().AutoDecrypt(b.InnerText);
                             LogText = LogText + x;
                             //LogText = LogText + System.Text.Encoding.UTF8.GetString(x);
@@ -119,9 +120,13 @@ namespace UptimeMonitorLib
                             break;
                     }
                 }
-
-                Trace.WriteLine(LogText);
             }
+            catch (Exception ex)
+            {
+                LogText = "Exception caught while executing Timer event:\n" + ex.Message;
+            }
+
+            Trace.WriteLine(LogText);
         }
 
     }
