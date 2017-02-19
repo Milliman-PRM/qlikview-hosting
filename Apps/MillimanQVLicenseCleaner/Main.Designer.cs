5cb774456d303cf00731d9711c5eb606c9108114
Van W Nanney <van.nanney@milliman.com>
2017-02-19 14:44:22 -0500

    UI tweak: key settings and help
    Added key settings window and help window. Limited number of license resets via configuration item
    
    
    Former-commit-id: 3c9ed0ae121a412337e81dec8f25098da2dc92b1

--- Apps/MillimanQVLicenseCleaner/Main.Designer.cs
+++ Apps/MillimanQVLicenseCleaner/Main.Designer.cs
@@ -29,45 +29,48 @@
         private void InitializeComponent()
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.button2 = new System.Windows.Forms.Button();
+            this.CleanNow = new System.Windows.Forms.Button();
             this.LicenseGrid = new System.Windows.Forms.DataGridView();
             this.Delete = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             this.LastAccess = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.LicenseType = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.UserName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Document = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Settings = new System.Windows.Forms.ListBox();
+            this.Help = new System.Windows.Forms.RichTextBox();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.LicenseGrid)).BeginInit();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
-            this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Controls.Add(this.button2, 0, 1);
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
             this.tableLayoutPanel1.Controls.Add(this.LicenseGrid, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.CleanNow, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.Settings, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.Help, 1, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(639, 332);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
-            // button2
+            // CleanNow
             // 
-            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.tableLayoutPanel1.SetColumnSpan(this.button2, 2);
-            this.button2.Location = new System.Drawing.Point(259, 303);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(121, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Clean QV License";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.CleanNow.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.CleanNow.Location = new System.Drawing.Point(503, 270);
+            this.CleanNow.Name = "CleanNow";
+            this.CleanNow.Size = new System.Drawing.Size(121, 23);
+            this.CleanNow.TabIndex = 1;
+            this.CleanNow.Text = "Clean QV License";
+            this.CleanNow.UseVisualStyleBackColor = true;
+            this.CleanNow.Click += new System.EventHandler(this.RemoveLicenseNow);
             // 
             // LicenseGrid
             // 
@@ -80,45 +83,58 @@
             this.LicenseType,
             this.UserName,
             this.Document});
-            this.tableLayoutPanel1.SetColumnSpan(this.LicenseGrid, 2);
+            this.tableLayoutPanel1.SetColumnSpan(this.LicenseGrid, 3);
             this.LicenseGrid.Dock = System.Windows.Forms.DockStyle.Fill;
             this.LicenseGrid.Location = new System.Drawing.Point(3, 3);
             this.LicenseGrid.Name = "LicenseGrid";
-            this.LicenseGrid.ReadOnly = true;
-            this.LicenseGrid.Size = new System.Drawing.Size(633, 291);
+            this.LicenseGrid.Size = new System.Drawing.Size(633, 226);
             this.LicenseGrid.TabIndex = 2;
             // 
             // Delete
             // 
             this.Delete.HeaderText = "Delete";
             this.Delete.Name = "Delete";
-            this.Delete.ReadOnly = true;
             this.Delete.Width = 75;
             // 
             // LastAccess
             // 
             this.LastAccess.HeaderText = "Last Accessed";
             this.LastAccess.Name = "LastAccess";
-            this.LastAccess.ReadOnly = true;
             // 
             // LicenseType
             // 
             this.LicenseType.HeaderText = "License Type";
             this.LicenseType.Name = "LicenseType";
-            this.LicenseType.ReadOnly = true;
             // 
             // UserName
             // 
             this.UserName.HeaderText = "Account";
             this.UserName.Name = "UserName";
-            this.UserName.ReadOnly = true;
             // 
             // Document
             // 
             this.Document.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.Document.HeaderText = "Document";
             this.Document.Name = "Document";
-            this.Document.ReadOnly = true;
+            // 
+            // Settings
+            // 
+            this.Settings.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Settings.FormattingEnabled = true;
+            this.Settings.Location = new System.Drawing.Point(3, 235);
+            this.Settings.Name = "Settings";
+            this.Settings.Size = new System.Drawing.Size(144, 94);
+            this.Settings.TabIndex = 3;
+            // 
+            // Help
+            // 
+            this.Help.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Help.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Help.Location = new System.Drawing.Point(153, 235);
+            this.Help.Name = "Help";
+            this.Help.Size = new System.Drawing.Size(333, 94);
+            this.Help.TabIndex = 4;
+            this.Help.Text = "";
             // 
             // Main
             // 
@@ -137,13 +153,15 @@
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button CleanNow;
         private System.Windows.Forms.DataGridView LicenseGrid;
         private System.Windows.Forms.DataGridViewCheckBoxColumn Delete;
         private System.Windows.Forms.DataGridViewTextBoxColumn LastAccess;
         private System.Windows.Forms.DataGridViewTextBoxColumn LicenseType;
         private System.Windows.Forms.DataGridViewTextBoxColumn UserName;
         private System.Windows.Forms.DataGridViewTextBoxColumn Document;
+        private System.Windows.Forms.ListBox Settings;
+        private System.Windows.Forms.RichTextBox Help;
     }
 }
 
--- Apps/MillimanQVLicenseCleaner/Main.cs
+++ Apps/MillimanQVLicenseCleaner/Main.cs
@@ -19,12 +19,17 @@ namespace MillimanQVLicenseCleaner
             string[] Args = Environment.GetCommandLineArgs();
             foreach (string Arg in Args)
             {
-                if (string.Compare(Arg, "AutoRun", true) == 0)
+                if (Arg.ToLower().IndexOf("runsilent") != -1 )
                     AutoRun = true;
                 
             }
             MaxAgeInDays = System.Convert.ToInt32(System.Configuration.ConfigurationManager.AppSettings["MaxAgeInDays"]);
+            string MaxToRemove = System.Configuration.ConfigurationManager.AppSettings["MaxToRemove"];
 
+            Settings.Items.Add("MaxAgeInDays=" + MaxAgeInDays.ToString());
+            Settings.Items.Add("MaxToRemove=" + MaxToRemove);
+            this.Text = "QV License Cleaner";
+            Help.Text = "MaxToRemove key is used to limit the number of users that automatically qualify for license removal. By manually selecting users,  more license can be cleared than specified by the limit.  Any user who's license is less that 48 hours old will have thier license quarantined and will not be able to access the document associated with the license until the quaratine period has expired.";
             if (AutoRun == false)
             {
                 LoadDisplay();
@@ -44,19 +49,30 @@ namespace MillimanQVLicenseCleaner
 
         private void LoadDisplay()
         {
+            int MaxToRemove = System.Convert.ToInt32(System.Configuration.ConfigurationManager.AppSettings["MaxToRemove"]);
+            int RemovalCount = 0;
             LicenseGrid.Rows.Clear();
             MillimanReportReduction.QVLicensing QVL = new MillimanReportReduction.QVLicensing();
             List<MillimanReportReduction.QVLicensing.LicenseInfo> AllLicense = QVL.GetAllCALSOfTypeInUse(MillimanReportReduction.QVLicensing.LicenseTypeEnum.ANY);
             foreach( MillimanReportReduction.QVLicensing.LicenseInfo LI in AllLicense )
             {
                 TimeSpan TS = DateTime.Now - LI.LastAccessed;
-                bool IsDelateCandidate = TS.Days > MaxAgeInDays ? true : false;
+                bool IsDelateCandidate = false;
+                if ( ( TS.Days > MaxAgeInDays ) && (RemovalCount < MaxToRemove) )
+                {
+                    RemovalCount++;
+                    IsDelateCandidate = true;
+                }
                 AddRow(IsDelateCandidate, LI.LastAccessed.ToShortDateString(), LI.License.ToString(), LI.User, LI.DocumentName);
             }
         }
 
         private void CleanLicense(int MaxAge )
         {
+            List<string> RemovalList = new List<string>();
+            int DocCalCount = 0;
+            int NamedCalCount = 0;
+
             MillimanReportReduction.QVLicensing QVL = new MillimanReportReduction.QVLicensing();
 
             foreach ( DataGridViewRow DGVR in LicenseGrid.Rows)
@@ -68,14 +84,25 @@ namespace MillimanQVLicenseCleaner
                     string Document = DGVR.Cells["Document"].Value.ToString();
                     if (string.IsNullOrEmpty(User))
                     {
-                        MessageBox.Show("Request to delete " + LicenseType + "  CAL with no given account value");
+                        if (AutoRun == false)
+                        {
+                            MessageBox.Show("Request to delete " + LicenseType + "  CAL with no given account value");
+                        }
                         continue;
                     }
 
                     if ( string.Compare(LicenseType, "NAMED", 0) == 0)
                     {
                         if (QVL.RemoveNamedUserCAL(User) == false)
+                        {
+                            if ( AutoRun == false)
                             MessageBox.Show("Failed to delete named CAL for user:" + User);
+                        }
+                        else
+                        {
+                            RemovalList.Add(User + " NAMED LICENSE removed" + Environment.NewLine);
+                            NamedCalCount++;
+                        }
                     }
                     else
                     {
@@ -86,14 +113,27 @@ namespace MillimanQVLicenseCleaner
                         }
                         if (QVL.RemoveUserDocCAL(User, Document) == false)
                             MessageBox.Show("Failed to delete doc cal for user:" + User + "  document:" + Document);
+                        else
+                        {
+                            RemovalList.Add(User + " DOC LICENSE removed" + Environment.NewLine);
+                            DocCalCount++;
+                        }
                     }
                 }
             }
-            LoadDisplay();
 
+            if ( AutoRun == false )
+                LoadDisplay();
+
+            string RemovalMsg = "Removed " + NamedCalCount.ToString() + " NAMED CALs" + Environment.NewLine + "Removed " + DocCalCount.ToString() + " DOC CALs" + Environment.NewLine + Environment.NewLine;
+            foreach (string Msg in RemovalList)
+                RemovalMsg += Msg;
+
+            if (AutoRun == false)
+                MessageBox.Show(RemovalMsg, "Status");
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void RemoveLicenseNow(object sender, EventArgs e)
         {
             CleanLicense(MaxAgeInDays);
         }
