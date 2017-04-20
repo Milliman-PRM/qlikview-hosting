49d17bcad24993212e71229da1a91b1274584bfd
Ben Wyatt <Ben.Wyatt@milliman.com>
2017-04-20 13:33:40 -0400

    Refactored GUI app into a separate VS project

--- WebApps/MillFrame/PRMServerMonitorGUI/Program.cs
+++ WebApps/MillFrame/PRMServerMonitorGUI/Program.cs
@@ -1,15 +1,12 @@
-﻿/*
- * CODE OWNERS: Tom Puckett, 
- * OBJECTIVE: Various PRM system monitoring (and possibly maintenance in the future).
- * DEVELOPER NOTES: This is fundamentally a Windows Forms application but can mimic the operation of a console application as determined below
- */
-
-using System;
-using System.Diagnostics;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Diagnostics;
+using System.Threading.Tasks;
 using System.Windows.Forms;
+using PrmServerMonitor;
 
-namespace PrmServerMonitor
+namespace PRMServerMonitorGUI
 {
     static class Program
     {
--- WebApps/MillFrame/PRMServerMonitorGUI/RunPrmServerMonitor.bat
+++ WebApps/MillFrame/PRMServerMonitorGUI/RunPrmServerMonitor.bat
@@ -4,4 +4,4 @@ REM  * OBJECTIVE: Batch file for launching the PrmServerMonitor application non-
 REM  * DEVELOPER NOTES: The `start /nowait` launch mechanism causes the console from which the application is launched to block pending program completion
 REM  */
 
-start /wait PrmServerMonitor.exe console orphantaskremoval managecals reportnamedcalassigned reportnamedcallimit reportdocumentcalassigned reportdocumentcallimit
+start /wait PrmServerMonitorGUI.exe console orphantaskremoval managecals reportnamedcalassigned reportnamedcallimit reportdocumentcalassigned reportdocumentcallimit
