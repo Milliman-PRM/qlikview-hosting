7980521d0fe67c6c298205b325368ceb0b10c738
Ben Wyatt <Ben.Wyatt@milliman.com>
2017-05-10 13:50:07 -0400

    Convert to a more robust powershell script
    Use PowerShell cmdlets for all work, add exception handling, and output deployed URLs to a text file

--- Common/CI_Publishing/CI_Publish_post_steps.ps1
+++ Common/CI_Publishing/CI_Publish_post_steps.ps1
@@ -1,20 +1,51 @@
-C:\Windows\SysWOW64\inetsrv\appcmd.exe add app /site.name:"Default Web Site" /path:"/prm_ci_<<branch_name>>_ClientPublisher" /physicalPath:"D:\installedapplications\prm_ci\<<branch_name>>\ClientPublisher" /applicationPool:"Qlikview IIS" 
+$outputPath = "D:\installedapplications\prm_ci\<<branch_name>>\error.log"
+$urlFilePath = "D:\installedapplications\prm_ci\<<branch_name>>\urls.log"
+$urlBase = "https://indy-prm-2.milliman.com"
+$errorCode = 0
 
-C:\Windows\SysWOW64\inetsrv\appcmd.exe add app /site.name:"Default Web Site" /path:"/prm_ci_<<branch_name>>_ClientUserAdmin" /physicalPath:"D:\installedapplications\prm_ci\<<branch_name>>\ClientUserAdmin" /applicationPool:"Qlikview IIS"
+# Clear URL file, if it exists
+set-content -LiteralPath $urlFilePath "Published URLs:"
 
-C:\Windows\SysWOW64\inetsrv\appcmd.exe add app /site.name:"Default Web Site" /path:"/prm_ci_<<branch_name>>_Milliman" /physicalPath:"D:\installedapplications\prm_ci\<<branch_name>>\Milliman" /applicationPool:"Qlikview IIS" 
+# (Re-)create applications and log deployed URLs to text file
+try 
+{
+    $apps = @{
+                "/prm_ci_<<branch_name>>_ClientPublisher" = "D:\installedapplications\prm_ci\<<branch_name>>\ClientPublisher";
+                "/prm_ci_<<branch_name>>_ClientUserAdmin" = "D:\installedapplications\prm_ci\<<branch_name>>\ClientUserAdmin";
+                "/prm_ci_<<branch_name>>_Milliman" = "D:\installedapplications\prm_ci\<<branch_name>>\Milliman";
+                "/prm_ci_<<branch_name>>_MillimanServices" = "D:\installedapplications\prm_ci\<<branch_name>>\MillimanServices";
+                "/prm_ci_<<branch_name>>_MillimanSupport" = "D:\installedapplications\prm_ci\<<branch_name>>\MillimanSupport";
+                "/prm_ci_<<branch_name>>_ProjectManagementConsoleServices" = "D:\installedapplications\prm_ci\<<branch_name>>\ProjectManagementConsoleServices";
+                "/prm_ci_<<branch_name>>_ProjectManConsole" = "D:\installedapplications\prm_ci\<<branch_name>>\ProjectManConsole";
+                "/prm_ci_<<branch_name>>_SQLBrowser" = "D:\installedapplications\prm_ci\<<branch_name>>\SQLBrowser";
+                "/prm_ci_<<branch_name>>_WEBTestHarness" = "D:\installedapplications\prm_ci\<<branch_name>>\WEBTestHarness"
+                }
 
-C:\Windows\SysWOW64\inetsrv\appcmd.exe set config /commit:WEBROOT /section:system.web/authentication /forms.loginURL:UserLogin.aspx
+    foreach ($app in $apps.GetEnumerator())
+    {
+        New-WebApplication -Force -Name $app.Name -PhysicalPath $app.Value -Site "Default Web Site" -ApplicationPool "Qlikview IIS"
+        Add-Content -LiteralPath $urlFilePath ($urlBase + $app.Name + "/")
+    }
+}
+catch 
+{
+    $errorCode = 1
+    Set-Content -LiteralPath $outputPath $errorCode
+}
 
-C:\Windows\SysWOW64\inetsrv\appcmd.exe add app /site.name:"Default Web Site" /path:"/prm_ci_<<branch_name>>_MillimanServices" /physicalPath:"D:\installedapplications\prm_ci\<<branch_name>>\MillimanServices" /applicationPool:"Qlikview IIS"  
+# Set the Forms Authentication loginURL property for Milliman application
+try
+{
+    set-webconfigurationproperty -PSPath "MACHINE/WEBROOT/APPHOST/Default Web Site/prm_ci_<<branch_name>>_Milliman" -Filter "system.web/authentication/forms" -name loginURL -value "UserLogin.aspx"
+}
+catch
+{
+    $errorCode = 2
+    Set-Content -LiteralPath $outputPath $errorCode
+}
 
-C:\Windows\SysWOW64\inetsrv\appcmd.exe add app /site.name:"Default Web Site" /path:"/prm_ci_<<branch_name>>_MillimanSupport" /physicalPath:"D:\installedapplications\prm_ci\<<branch_name>>\MillimanSupport" /applicationPool:"Qlikview IIS" 
-
-C:\Windows\SysWOW64\inetsrv\appcmd.exe add app /site.name:"Default Web Site" /path:"/prm_ci_<<branch_name>>_ProjectManagementConsoleServices" /physicalPath:"D:\installedapplications\prm_ci\<<branch_name>>\ProjectManagementConsoleServices" /applicationPool:"Qlikview IIS" 
-
-C:\Windows\SysWOW64\inetsrv\appcmd.exe add app /site.name:"Default Web Site" /path:"/prm_ci_<<branch_name>>_ProjectManConsole" /physicalPath:"D:\installedapplications\prm_ci\<<branch_name>>\ProjectManConsole" /applicationPool:"Qlikview IIS" 
-
-C:\Windows\SysWOW64\inetsrv\appcmd.exe add app /site.name:"Default Web Site" /path:"/prm_ci_<<branch_name>>_SQLBrowser" /physicalPath:"D:\installedapplications\prm_ci\<<branch_name>>\SQLBrowser" /applicationPool:"Qlikview IIS" 
-
-C:\Windows\SysWOW64\inetsrv\appcmd.exe add app /site.name:"Default Web Site" /path:"/prm_ci_<<branch_name>>_WEBTestHarness" /physicalPath:"D:\installedapplications\prm_ci\<<branch_name>>\WEBTestHarness" /applicationPool:"Qlikview IIS" 
-
+# If we've reached this point and $errorCode is still 0, everything was fine
+if ($errorCode -eq 0)
+{
+    Set-Content -LiteralPath $outputPath $errorCode
+}
