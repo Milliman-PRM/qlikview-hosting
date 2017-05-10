fb010f17f96f1791283f1997ae6412a11b1e5eb8
tom.puckett <tom.puckett@milliman.com>
2017-05-10 10:55:58 -0400

    Expanded the width of user id box for login pages in portal and client publisher applications.

--- WebApps/MillFrame/Milliman/Milliman/UserLogin.aspx.designer.cs
+++ WebApps/MillFrame/Milliman/Milliman/UserLogin.aspx.designer.cs
@@ -67,6 +67,15 @@ namespace MillimanDev {
         protected global::System.Web.UI.WebControls.Label lblErrorMessage;
         
         /// <summary>
+        /// lblcopyrightYear control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblcopyrightYear;
+        
+        /// <summary>
         /// spToIdPBindingList control.
         /// </summary>
         /// <remarks>
