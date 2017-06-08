f02bf2a0dd7e35e601800164a31e7d57a0e9f54a
Ben Wyatt <Ben.Wyatt@milliman.com>
2017-06-08 14:14:15 -0400

    Add redirect to modal user management pages

--- WebApps/MillFrame/MillimanUserAdmin/admin/Edit_user_modal_success.aspx.cs
+++ WebApps/MillFrame/MillimanUserAdmin/admin/Edit_user_modal_success.aspx.cs
@@ -4,6 +4,10 @@ public partial class admin_Edit_user_modal_success : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        // Ensure user is currently authenticated to the application. If not, redirect back to login page.
+        if (!Page.Request.IsAuthenticated)
+        {
+            Response.Redirect(MapPath("~/login.aspx"));
+        }
     }
 }
