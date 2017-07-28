85cc4e19e849884a7e87901737644166431b9358
tom.puckett <tom.puckett@milliman.com>
2017-07-28 09:19:05 -0400

    Add utility DehexifyStrings

---
+++ Apps/DehexifyStrings/DehexifyStrings/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace DehexifyStrings
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonFromFile = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.TextBoxResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ButtonFromFile
+            // 
+            this.ButtonFromFile.Location = new System.Drawing.Point(13, 13);
+            this.ButtonFromFile.Name = "ButtonFromFile";
+            this.ButtonFromFile.Size = new System.Drawing.Size(75, 23);
+            this.ButtonFromFile.TabIndex = 0;
+            this.ButtonFromFile.Text = "Read File";
+            this.ButtonFromFile.UseVisualStyleBackColor = true;
+            this.ButtonFromFile.Click += new System.EventHandler(this.ButtonFromFile_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // TextBoxResult
+            // 
+            this.TextBoxResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TextBoxResult.Location = new System.Drawing.Point(12, 67);
+            this.TextBoxResult.Multiline = true;
+            this.TextBoxResult.Name = "TextBoxResult";
+            this.TextBoxResult.Size = new System.Drawing.Size(651, 421);
+            this.TextBoxResult.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(675, 500);
+            this.Controls.Add(this.TextBoxResult);
+            this.Controls.Add(this.ButtonFromFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonFromFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox TextBoxResult;
+    }
+}
+
---
+++ Apps/DehexifyStrings/DehexifyStrings/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace DehexifyStrings
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonFromFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string FileName = openFileDialog1.FileName;
+
+                using (StreamReader Reader = new StreamReader(FileName))
+                {
+                    string Line;
+                    while (Reader.Peek() > 0)
+                    {
+                        Line = Reader.ReadLine();
+                        TextBoxResult.Text += HexToString(Line) + "\r\n";
+                    }
+                }
+            }
+        }
+
+        private string HexToString(string Input)
+        {
+            if (!Regex.IsMatch(Input, @"^[a-fA-F0-9]+$"))
+            {
+                return null;
+            }
+
+            string ReturnVal = string.Empty;
+
+            while (Input.Length > 1)
+            {
+                string NextCharHex = Input.Substring(0, 2);
+                Input = Input.Substring(2);
+                ReturnVal += System.Convert.ToChar(Convert.ToUInt16(NextCharHex, 16));
+            }
+
+            return ReturnVal;
+        }
+    }
+}
