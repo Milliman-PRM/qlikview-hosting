eb5900bdeeddf6341371649c09a0f789ee99d7e7
tom.puckett <tom.puckett@milliman.com>
2017-07-31 21:13:47 -0400

    Work in progress on selections viewer

--- Apps/DehexifyStrings/DehexifyStrings/Form1.Designer.cs
+++ Apps/DehexifyStrings/DehexifyStrings/Form1.Designer.cs
@@ -31,11 +31,20 @@
             this.ButtonFromFile = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.TextBoxResult = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.ButtonReadSelections = new System.Windows.Forms.Button();
+            this.TextBoxSelections = new System.Windows.Forms.TextBox();
+            this.TreeViewHierarchy = new System.Windows.Forms.TreeView();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // ButtonFromFile
             // 
-            this.ButtonFromFile.Location = new System.Drawing.Point(13, 13);
+            this.ButtonFromFile.Location = new System.Drawing.Point(6, 6);
             this.ButtonFromFile.Name = "ButtonFromFile";
             this.ButtonFromFile.Size = new System.Drawing.Size(75, 23);
             this.ButtonFromFile.TabIndex = 0;
@@ -52,23 +61,96 @@
             this.TextBoxResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.TextBoxResult.Location = new System.Drawing.Point(12, 67);
+            this.TextBoxResult.Location = new System.Drawing.Point(6, 35);
             this.TextBoxResult.Multiline = true;
             this.TextBoxResult.Name = "TextBoxResult";
-            this.TextBoxResult.Size = new System.Drawing.Size(651, 421);
+            this.TextBoxResult.Size = new System.Drawing.Size(630, 408);
             this.TextBoxResult.TabIndex = 1;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(13, 13);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(650, 475);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.ButtonFromFile);
+            this.tabPage1.Controls.Add(this.TextBoxResult);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(642, 449);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Raw File";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.TreeViewHierarchy);
+            this.tabPage2.Controls.Add(this.TextBoxSelections);
+            this.tabPage2.Controls.Add(this.ButtonReadSelections);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(642, 449);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "User Selections";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // ButtonReadSelections
+            // 
+            this.ButtonReadSelections.Location = new System.Drawing.Point(7, 7);
+            this.ButtonReadSelections.Name = "ButtonReadSelections";
+            this.ButtonReadSelections.Size = new System.Drawing.Size(75, 23);
+            this.ButtonReadSelections.TabIndex = 0;
+            this.ButtonReadSelections.Text = "Browse...";
+            this.ButtonReadSelections.UseVisualStyleBackColor = true;
+            this.ButtonReadSelections.Click += new System.EventHandler(this.ButtonReadSelections_Click);
+            // 
+            // TextBoxSelections
+            // 
+            this.TextBoxSelections.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TextBoxSelections.Location = new System.Drawing.Point(442, 37);
+            this.TextBoxSelections.Multiline = true;
+            this.TextBoxSelections.Name = "TextBoxSelections";
+            this.TextBoxSelections.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TextBoxSelections.Size = new System.Drawing.Size(194, 406);
+            this.TextBoxSelections.TabIndex = 1;
+            // 
+            // TreeViewHierarchy
+            // 
+            this.TreeViewHierarchy.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TreeViewHierarchy.CheckBoxes = true;
+            this.TreeViewHierarchy.Location = new System.Drawing.Point(7, 37);
+            this.TreeViewHierarchy.Name = "TreeViewHierarchy";
+            this.TreeViewHierarchy.Size = new System.Drawing.Size(429, 406);
+            this.TreeViewHierarchy.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(675, 500);
-            this.Controls.Add(this.TextBoxResult);
-            this.Controls.Add(this.ButtonFromFile);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -77,6 +159,12 @@
         private System.Windows.Forms.Button ButtonFromFile;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.TextBox TextBoxResult;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox TextBoxSelections;
+        private System.Windows.Forms.Button ButtonReadSelections;
+        private System.Windows.Forms.TreeView TreeViewHierarchy;
     }
 }
 
--- Apps/DehexifyStrings/DehexifyStrings/Form1.cs
+++ Apps/DehexifyStrings/DehexifyStrings/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml;
 using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -37,6 +38,54 @@ namespace DehexifyStrings
             }
         }
 
+        private XmlDocument DecodeHierarchyFile(string FileName)
+        {
+            if (!File.Exists(FileName))
+            {
+                return null;
+            }
+
+            XmlDocument XMLD = new XmlDocument();
+            string XML = "<xml>" + System.IO.File.ReadAllText(FileName) + "</xml>";
+            XMLD.LoadXml(XML);
+
+            DecodeXmlNode(XMLD.SelectSingleNode("xml"));
+
+            return XMLD;
+        }
+
+        private XmlNode DecodeXmlNode(XmlNode EncodedNode)
+        {
+            if (EncodedNode.Attributes.Count == 2)
+            {
+                string EncodedText = EncodedNode.Attributes["Text"].Value;
+                string EncodedValue = EncodedNode.Attributes["Value"].Value;
+
+                EncodedNode.Attributes["Value"].Value = DecodeDelimitedString(EncodedValue, '|');
+                EncodedNode.Attributes["Text"].Value = DecodeDelimitedString(EncodedText, '|');
+            }
+
+            foreach (XmlNode Child in EncodedNode.ChildNodes.OfType<XmlElement>())
+            {
+                DecodeXmlNode(Child);
+            }
+
+            return EncodedNode;
+        }
+
+        string DecodeDelimitedString(string EncodedString, char Delimiter)
+        {
+            string ReturnVal = string.Empty;
+            string[] InputArray = EncodedString.Split(Delimiter);
+            List<string> OutputList = new List<string>();
+            foreach (string EncodedElement in InputArray)
+            {
+                OutputList.Add(HexToString(EncodedElement));
+            }
+            ReturnVal = string.Join(Delimiter.ToString(), OutputList);
+            return ReturnVal;
+        }
+
         private string HexToString(string Input)
         {
             if (!Regex.IsMatch(Input, @"^[a-fA-F0-9]+$"))
@@ -54,6 +103,70 @@ namespace DehexifyStrings
             }
 
             return ReturnVal;
+        }
+
+        private void XmlToTree(XmlDocument Dom)
+        {
+            TreeViewHierarchy.Nodes.Clear();
+
+            TreeViewHierarchy.Nodes.Add(new TreeNode(Dom.DocumentElement.Name));
+            TreeNode TNode = TreeViewHierarchy.Nodes[0];
+
+            AddTreeNode(Dom.DocumentElement, TNode);
+        }
+
+        private void AddTreeNode(XmlNode NodeToAdd, TreeNode InTreeNode)
+        {
+            for (int i = 0; i <= NodeToAdd.ChildNodes.Count - 1; i++)
+            {
+                //TreeNode NewTreeNode = new TreeNode("");
+                XmlNode XNode = NodeToAdd.ChildNodes[i];
+                InTreeNode.Nodes.Add(new TreeNode(XNode.Name));
+                TreeNode TNode = InTreeNode.Nodes[i];
+                AddTreeNode(XNode, TNode);
+            }
+
+            if (NodeToAdd.Attributes.GetNamedItem("Value") != null && NodeToAdd.Attributes.GetNamedItem("Text") != null)
+            {
+                InTreeNode.Text = NodeToAdd.Attributes["Value"].Value + " - " + NodeToAdd.Attributes["Text"].Value;
+            }
+        }
+
+        private void ButtonReadSelections_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Open Selections file";
+            openFileDialog1.Filter = "Selection files (*.selections)|*.selections|All files (*.*)|*.*";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK && File.Exists(openFileDialog1.FileName))
+            {
+                string HierarchyFolder = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(openFileDialog1.FileName), "..\\.."));
+                string HierarchyFile = Directory.GetFiles(HierarchyFolder, "*.hierarchy_*").First();
+
+                XmlDocument HierarchyDocument = DecodeHierarchyFile(HierarchyFile);
+                XmlToTree(HierarchyDocument);
+
+                using (StreamReader Reader = new StreamReader(openFileDialog1.FileName))
+                {
+                    while (Reader.Peek() > 0)
+                    {
+                        string Line = Reader.ReadToEnd();
+
+                        XmlDocument XMLD = new XmlDocument();
+                        string XML = System.IO.File.ReadAllText(openFileDialog1.FileName);
+                        XMLD.LoadXml(XML);
+
+                        foreach (XmlNode Selection in XMLD.SelectSingleNode("Collection").SelectSingleNode("Items").SelectNodes("Simple"))
+                        {
+                            string[] Selected = Selection.Attributes["value"].Value.Split('|');
+                            string One = Selected[1];
+
+                            TreeNode[] x = TreeViewHierarchy.Nodes.Find(One, false);
+                        }
+
+                        //var x = TreeViewHierarchy.Nodes.Find(SelectedLeaf[1], false);
+                    }
+                }
+            }
         }
     }
 }
